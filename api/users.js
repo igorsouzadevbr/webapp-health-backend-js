5aabddd7aa28135203408769d08b0717eb9ca156
Igor Souza <igor@cadenzatecnologia.com.br>
2023-09-17 00:47:29 -0300

    Alterações Classe de Usuários - api

--- api/users.js
+++ api/users.js
@@ -145,25 +145,27 @@ class Users {
 
 
       create (req, res) {
-        const { name, email, password } = req.body;
+        const { name, email, phone, birthdate, gender, password} = req.body;
         const { v4: uuidv4 } = require('uuid');
         const uniqueid = uuidv4();
         const util = require('./util/util.js');
         
+        if (!util.isPhoneNumber(phone)) {return res.status(409).send({ message: 'O telefone informado não é válido.'});}
+        if (!util.isInteger(gender)) {return res.status(409).send({ message: 'O gênero informado não é um número.'});}
         if (!util.isEmail(email)) {return res.status(409).send({ message: 'O campo EMAIL informado não é um e-mail válido.'}); }
         this.connection.query("SELECT * FROM users where email = ?", [email], (err, results) => {
            if (err) {
              console.error('Erro ao verificar e-mail do usuário:', err);
              return res.sendStatus(500);
            }
-           if (results.length >0) { return res.status(409).send({ message: 'E-mail já existe.'}); }
+           if (results.length >0) { return res.status(409).send({ message: 'E-mail informado já existe.'}); }
    
          //FIM VERIFICAÇÕES
    
          // Inserção do usuário no banco de dados
-         const query = 'INSERT INTO users(uniqueid, name, email, password, usertype) VALUES (?, ?, ?, ?, ?)';
+         const query = 'INSERT INTO users(uniqueid, name, email, password, usertype, phone, birthdate, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
          try {
-         this.connection.query(query, [uniqueid, name, email, util.convertToSHA256(password), 0], (err, results) => {
+         this.connection.query(query, [uniqueid, name, email, util.convertToSHA256(password), 0, phone, birthdate, gender], (err, results) => {
            if (err) {
              console.error('Erro ao criar usuário:', err);
              return res.sendStatus(500);
