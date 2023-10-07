fb20b6f8ea91735096a0a3e01f72b75da00cb113
Igor Souza <igor@cadenzatecnologia.com.br>
2023-10-07 12:55:04 -0300

    Correção erro Cors

--- index.js
+++ index.js
@@ -53,6 +53,15 @@ connection.getConnection((err) => {
   }
 });
 
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 //ROTAS DE AUTENTICAÇÃO
 const clientLogin = new Login(secretTokenKey);
 app.post('/api/auth', (req, res) => {
