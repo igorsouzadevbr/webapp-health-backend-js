5d113f5b8547428fcba4b29a669a3d990ea17775
Igor Souza <igor@cadenzatecnologia.com.br>
2023-10-12 12:47:52 -0300

    aumentar limite de arquivos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const mysql = require('mysql2');
 const app = express();
+app.use(bodyParser.json({ limit: '5mb' }));
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const corsOptions = {
