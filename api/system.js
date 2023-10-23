f9c60bc3b1376efa832e35603aa941f81d26442e
Igor Souza <igor@cadenzatecnologia.com.br>
2023-10-23 11:26:58 -0300

    correções sistema

--- api/system.js
+++ api/system.js
@@ -14,162 +14,76 @@ class System {
     }
     return res.status(200).send({ userTypeData });
   }
-  getCityByID(req, res) {
+  async getCityByID(req, res) {
     const cityid = req.params.cityid;
     const databaseFramework = new dbUtils(this.connection);
 
-    this.connection.getConnection((err, connection) => {
-      if (err) {
-        connection.release(); console.error('Erro ao conectar ao banco de dados:', err.message); return;
-      }
+    const getCities = await databaseFramework.select("city", "*", "id = ?", [cityid]);
+    if (getCities.length === 0) {
+      return res.status(404).json({ message: 'Sem resultados.' });
+    }
+    return res.status(200).send({ getCities });
 
-      connection.query('SELECT * FROM city where id = ? and IsDeleted = 0', [cityid], (err, results) => {
-        connection.release();
-        if (err) {
-          connection.release();
-          console.error('Erro no método getCityByID, query n° 1:', err);
-          return res.sendStatus(500);
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ message: 'Sem resultados.' });
-        }
-        res.status(200).send({ results });
-
-      });
-    });
   }
-  getCityByName(req, res) {
-    const cityname = req.params.cityname
-    this.connection.getConnection((err, connection) => {
-      if (err) {
-        connection.release(); console.error('Erro ao conectar ao banco de dados:', err.message); return;
-      }
+  async getCityByName(req, res) {
+    const cityname = req.params.cityname;
+    const databaseFramework = new dbUtils(this.connection);
 
-      connection.query('SELECT * FROM city WHERE name = ? and IsDeleted = 0', [cityname], (err, results) => {
-        connection.release();
-        if (err) {
-          connection.release();
-          console.error('Erro no método getCityByName, query n° 1:', err);
-          return res.sendStatus(500);
-        }
-
-        if (results.length === 0) {
-          return res.status(404).json({ message: 'Sem resultados.' });
-        }
-        res.status(200).send({ results });
-
-      });
-    });
+    const getCities = await databaseFramework.select("city", "*", "name = ?", [cityname]);
+    if (getCities.length === 0) {
+      return res.status(404).json({ message: 'Sem resultados.' });
+    }
+    return res.status(200).send({ getCities });
   }
-  getStateByID(req, res) {
+  async getStateByID(req, res) {
     const stateid = req.params.stateid;
-    this.connection.getConnection((err, connection) => {
-      if (err) {
-        connection.release(); console.error('Erro ao conectar ao banco de dados:', err.message); return;
-      }
+    const databaseFramework = new dbUtils(this.connection);
 
-      connection.query('SELECT * FROM states where id = ? and IsDeleted = 0', [stateid], (err, results) => {
-        connection.release();
-        if (err) {
-          connection.release();
-          console.error('Erro no método getStateByID, query n° 1:', err);
-          return res.sendStatus(500);
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ message: 'Sem resultados.' });
-        }
-        res.status(200).send({ results });
-
-      });
-    });
+    const getStates = await databaseFramework.select("states", "*", "id = ?", [stateid]);
+    if (getStates.length === 0) {
+      return res.status(404).json({ message: 'Sem resultados.' });
+    }
+    return res.status(200).send({ getStates });
   }
-  getStateByAb(req, res) {
+  async getStateByAb(req, res) {
     const stateab = req.params.stateab;
-    this.connection.getConnection((err, connection) => {
-      if (err) {
-        connection.release(); console.error('Erro ao conectar ao banco de dados:', err.message); return;
-      }
+    const databaseFramework = new dbUtils(this.connection);
 
-      connection.query('SELECT * FROM states where tag = ? and IsDeleted = 0', [stateab], (err, results) => {
-        connection.release();
-        if (err) {
-          connection.release();
-          console.error('Erro no método getStateByAb, query n° 1:', err);
-          return res.sendStatus(500);
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ message: 'Sem resultados.' });
-        }
-        res.status(200).send({ results });
-
-      });
-    });
+    const getStates = await databaseFramework.select("states", "*", "tag = ?", [stateab]);
+    if (getStates.length === 0) {
+      return res.status(404).json({ message: 'Sem resultados.' });
+    }
+    return res.status(200).send({ getStates });
   }
-  getStateByName(req, res) {
+  async getStateByName(req, res) {
     const statename = req.params.statename
-    this.connection.getConnection((err, connection) => {
-      if (err) {
-        connection.release(); console.error('Erro ao conectar ao banco de dados:', err.message); return;
-      }
+    const databaseFramework = new dbUtils(this.connection);
 
-      connection.query('SELECT * FROM states WHERE nome = ? and IsDeleted = 0', [statename], (err, results) => {
-        connection.release();
-        if (err) {
-          connection.release();
-          console.error('Erro no método getStateByName, query n° 1:', err);
-          return res.sendStatus(500);
-        }
-
-        if (results.length === 0) {
-          return res.status(404).json({ message: 'Sem resultados.' });
-        }
-        res.status(200).send({ results });
-      });
-    });
+    const getStates = await databaseFramework.select("states", "*", "name = ?", [statename]);
+    if (getStates.length === 0) {
+      return res.status(404).json({ message: 'Sem resultados.' });
+    }
+    return res.status(200).send({ getStates });
   }
-  getGenderByID(req, res) {
+  async getGenderByID(req, res) {
     const genderid = req.params.genderid;
-    this.connection.getConnection((err, connection) => {
-      if (err) {
-        connection.release(); console.error('Erro ao conectar ao banco de dados:', err.message); return;
-      }
+    const databaseFramework = new dbUtils(this.connection);
 
-      connection.query('SELECT * FROM gender where id = ?', [genderid], (err, results) => {
-        connection.release();
-        if (err) {
-          connection.release();
-          console.error('Erro no método getGenderByID, query n° 1:', err);
-          return res.sendStatus(500);
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ message: 'Sem resultados.' });
-        }
-        res.status(200).send({ results });
-
-      });
-    });
+    const getGenders = await databaseFramework.select("gender", "*", "id = ?", [genderid]);
+    if (getGenders.length === 0) {
+      return res.status(404).json({ message: 'Sem resultados.' });
+    }
+    return res.status(200).send({ getGenders });
   }
-  getGenderByName(req, res) {
+  async getGenderByName(req, res) {
     const gendername = req.params.gendername;
-    this.connection.getConnection((err, connection) => {
-      if (err) {
-        connection.release(); console.error('Erro ao conectar ao banco de dados:', err.message); return;
-      }
+    const databaseFramework = new dbUtils(this.connection);
 
-      connection.query('SELECT * FROM gender where Name = ?', [gendername], (err, results) => {
-        connection.release();
-        if (err) {
-          connection.release();
-          console.error('Erro no método getGenderByName, query n° 1:', err);
-          return res.sendStatus(500);
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ message: 'Sem resultados.' });
-        }
-        res.status(200).send({ results });
-
-      });
-    });
+    const getGenders = await databaseFramework.select("gender", "*", "Name = ?", [gendername]);
+    if (getGenders.length === 0) {
+      return res.status(404).json({ message: 'Sem resultados.' });
+    }
+    return res.status(200).send({ getGenders });
   }
 
   async getPostalCode(req, res) {
