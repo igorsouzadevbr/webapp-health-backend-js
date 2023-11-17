fa061fa7a88bdc10804d00dbdc8b513652d6799f
Igor Souza <igor@cadenzatecnologia.com.br>
2023-11-17 19:28:10 -0300

    hotfix

--- api/chatFlow/attendantFlow.js
+++ api/chatFlow/attendantFlow.js
@@ -411,7 +411,7 @@ class chatAttendantFlow {
         const databaseFramework = new dbUtils(this.connection);
         try {
             const getSchedules = await databaseFramework.select("appointments", ["patient_id", "date", "start_time", "id"], "isConfirmed = 0 and professional_id = ?", [attendantId]);
-            if (getSchedules.length <= 0) { return res.status(404).json({ message: 'Não há agendamentos disponíveis.' }); }
+            if (getSchedules.length <= 0) { return res.status(200).json([]); }
 
             const patientData = [];
 
