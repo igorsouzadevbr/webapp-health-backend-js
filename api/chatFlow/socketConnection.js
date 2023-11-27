18a15f0054c425916a03d282e9af5ee6b3ef5d73
Igor Souza <igor@cadenzatecnologia.com.br>
2023-11-27 16:01:54 -0300

    feature rotina que finaliza chats smsg

--- api/chatFlow/socketConnection.js
+++ api/chatFlow/socketConnection.js
@@ -196,6 +196,40 @@ class SocketConnection {
 
     this.checkQueue();
     this.checkAndDeleteQueueItems();
+
+  }
+
+  async checkChatsWithNoMessages() {
+    setInterval(async () => {
+      try {
+        const databaseFramework = new dbUtils(this.connection);
+        const currentDate = moment();
+        const tenMinutesAgo = currentDate.clone().subtract(10, 'minutes');
+
+        const chatSessions = databaseFramework.select('chat_sessions', '*', 'finished = 0');
+        if (chatSessions.length >= 1) {
+          const getChatMessages = await databaseFramework.select("chat_messages", "*", "chat_session_id IN (?) and created_at <= ?", [chatSessions.id, tenMinutesAgo]);
+
+          for (const chatMessage of getChatMessages) {
+
+            if (chatMessage.length <= 0) {
+              const getChatSessionsId = await databaseFramework.select("chat_sessions", "*", "id =?", [chatMessage.chat_session_id]);
+              const chatSessionData = getChatSessionsId[0];
+
+              await databaseFramework.update("chat_sessions", { finished: 1 }, `id = ${chatSessionData.id}`);
+              await databaseFramework.update("chat_queue", { finished: 1 }, `id = ${chatSessionData.chat_queue_id}`);
+
+              this.io.emit('finishedChat', { chatId: chatMessage.chat_session_id, finished: 1 });
+            }
+
+          }
+        }
+
+
+      } catch (error) {
+        console.error('Erro na verificação e exclusão da fila:', error);
+      }
+    }, 1 * 60 * 1000);
   }
 
   async checkAndDeleteQueueItems() {
