24a9a400e897db14086240164df6cb044a33790b
Igor Souza <igor@cadenzatecnologia.com.br>
2023-11-29 17:16:00 -0300

    hotfix

--- api/chatFlow/socketConnection.js
+++ api/chatFlow/socketConnection.js
@@ -483,9 +483,10 @@ class SocketConnection {
         const chatSessions = databaseFramework.select('chat_sessions', '*', 'finished = 0');
         if (chatSessions.length >= 1) {
           for (const chatSession of chatSessions) {
-            const getChatMessages = await databaseFramework.select("chat_messages", "*", "chat_session_id = ? and created_at <= ?", [chatSession.id, tenMinutesAgo]);
+            const getChatMessages = await databaseFramework.select("chat_messages", "*", "chat_session_id = ? and created_at >= ?", [chatSession.id, tenMinutesAgo]);
 
             if (getChatMessages.length === 0) {
+              await databaseFramework.update("chat_attendants", { isOnChat: 0 }, `attendant_id = ${chatSession.attendant_id}`);
               await databaseFramework.update("chat_sessions", { finished: 1 }, `id = ${chatSession.id}`);
               await databaseFramework.update("chat_queue", { finished: 1 }, `id = ${chatSession.chat_queue_id}`);
               this.io.emit('finishService', { chatId: chatSession.id, finished: 1 });
@@ -495,7 +496,7 @@ class SocketConnection {
       } catch (error) {
         console.error('Erro na verificação e exclusão da fila:', error);
       }
-    }, 2 * 60 * 1000);
+    }, 1000);
   }
 
   async checkQttOfAttendantSchedules() {
